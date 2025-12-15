0c8a947b33f2a9c22e95668465cfca0353957d95
Jainam Jain <jainjainam412@gmail.com>
2025-12-15 23:52:59 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -57,3 +57,11 @@ export interface AgentResponse {
   model: ModelType;
   text: string;
 }
+export interface UploadedFile {
+  id: string;
+  name: string;
+  type: string;
+  size: number;
+  url: string;
+  base64?: string;
+}
